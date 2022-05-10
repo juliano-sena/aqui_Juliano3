59eb0d86298c684f1b792e6bb2fbb439ecca7c27
Juliano Sena do Nascimento <90390885+juliano-sena@users.noreply.github.com>
2022-05-10 09:55:20 -0300

    Delete darede-front directory

--- darede-front/src/services/auth.js
+++
@@ -1,8 +0,0 @@
-export const usuarioAutenticacao = () => localStorage.getItem('usuario-login') !== null;
-
-export const parseJWT = () => {
-
-    let base64 = localStorage.getItem('usuario-login').split('.')[1];
-
-    return JSON.parse(window.atob(base64));
-}
